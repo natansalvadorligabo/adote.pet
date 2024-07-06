65401c82c7c38f106e8a19bad15398cbc7af6271
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-07-05 21:32:43 -0300

    feat (model util user) : Encoder class for sensitive data, UserNotFoundException definition, UserLogin class for user validation(needs not implemented UsersReader class). feat (servlets) : UserLoginServlet implementation (needs to alter jsp home page path).

---
+++ src/main/java/ifsp/arq/tsi/web1/adotepet/model/util/user/UserLogin.java
@@ -0,0 +1,19 @@
+package ifsp.arq.tsi.web1.adotepet.model.util.user;
+import java.util.List;
+import ifsp.arq.tsi.web1.adotepet.model.User;
+
+public class UserLogin {
+
+	private UserLogin() {}
+
+	public static User login(String email, String password) {
+		List<User> users = UsersReader.read();
+
+		if (users != null)
+			for (User user : users)
+				if (user.getEmail().equals(email) && user.getPassword().equals(Encoder.encode(password)))
+					return user;
+		
+		throw new UserNotFoundException();
+	}
+}
---
+++ src/main/java/ifsp/arq/tsi/web1/adotepet/servlets/UserLoginServlet.java
@@ -0,0 +1,47 @@
+package ifsp.arq.tsi.web1.adotepet.servlets;
+
+import java.io.IOException;
+
+import ifsp.arq.tsi.web1.adotepet.model.User;
+import ifsp.arq.tsi.web1.adotepet.model.util.user.UserLogin;
+import ifsp.arq.tsi.web1.adotepet.model.util.user.UserNotFoundException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/login")
+public class UserLoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public UserLoginServlet() {
+		super();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+		
+		RequestDispatcher dispatcher = null;
+		
+		try {
+			User user = UserLogin.login(email, password);
+
+			req.setAttribute("user", user);
+
+			// TO DO: jsp home page path
+			dispatcher = req.getRequestDispatcher("/home-login.jsp");
+		}
+		catch(Exception generic){
+			req.setAttribute("result", generic.getMessage());
+			dispatcher = req.getRequestDispatcher("/home-login.jsp");
+		}
+
+		dispatcher.forward(req, resp);
+	}
+}
